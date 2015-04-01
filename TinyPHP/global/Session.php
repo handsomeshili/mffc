c39d4ace74cf4a4b3417fec93c9c47d1309aba98
handsomeshili <handsomeshili@gmail.com>
2015-04-01 11:39:07 +0800

    add

---
+++ TinyPHP/global/Session.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * class Session
+ * 对系统$_SESSION的封装
+ *
+ */
+
+class Session {
+
+    public function __construct() {}
+
+    public function init() {}
+
+    public function index() {}
+
+
+}
+
+
+
+ ?>
