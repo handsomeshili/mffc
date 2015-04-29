194fd502e1e0e1c9d59462c2ad2ad04c49e42976
handsomeshili <handsomeshili@gmail.com>
2015-04-29 10:47:31 +0800

    change config/ path

---
+++ app/config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * database config
+ * mysql
+ */
+
+return [
+        'driver'      => 'mysql',
+        'host'        => 'localhost',
+        'database'   => 'mffc',
+        'username'   => 'root',
+        'password'   => '1234',
+        'charset'    => 'utf8',
+        'collation'  => 'utf8_general_ci',
+        'prefix'      => ''
+    ];
+
+
+
+
+?>
---
+++ app/config/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * 路由分发文件,处理URL，指派对应的控制器处理该请求
+ * @author sily
+ *
+ */
+
+
+use NoahBuscher\Macaw\Macaw;
+
+
+Macaw::get('/(:all)', function($request_name) {
+    //从uri中获取module、controller、action
+    $request_name = trim($request_name);
+    $route = explode('/', $request_name);
+    // $route['param'] = $_GET;
+    require '../TinyPHP/Router.php';
+    Router::init($route);
+
+});
+
+//use 404 page as the not found tips
+Macaw::$error_callback = function() {
+    throw new Exception("路由无匹配项 404 Not Found");
+};
+
+Macaw::dispatch();
+?>
